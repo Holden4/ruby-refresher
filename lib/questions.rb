780d6316dce986e26f4799b169f25518da5e9e90
Rob Holden <robholdenflag@gmail.com>
2017-02-20 12:44:05 +0000

    A few more questions complete

--- lib/questions.rb
+++ lib/questions.rb
@@ -54,9 +54,8 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
-   half_it_to_float = (string.length.to_f) / 2
-   rounds_odd_numbers = (half_it_to_float.ceil).to_i
-   string[0, rounds_odd_numbers]
+   half_it_to_float = (string.length.to_f / 2).ceil
+   string[0, half_it_to_float]
    # Better way?
 end
 
@@ -86,7 +85,7 @@ end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
-   array.sort_by { |x| x.length }.first
+   array.sort_by { |x| x.length }.firsty.inject(0){ |sum, x| sum + x }
 end
 
 # return the shortest word in an array
@@ -97,21 +96,24 @@ end
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
-   array.inject(0){ |sum, x| sum + x }
+   array.reduce(:+)
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  array.insert(-1, array.to_s)
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  array.reduce(:+) / arra.size
 end
 
 # get all the elements in an array, up until the first element
@@ -125,6 +127,7 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  h = Hash[*array]
 end
 
 # get all the letters used in an array of words and return
@@ -132,43 +135,55 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+  var = array.join
+  var.split("").sort
+
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+  hash.invert
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+  values = hash.values.inject { |a, b| a + b }
+  keys = hash.keys.inject { |a, b| a + b }
+  values + keys
 end
 
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
+  string.tr("A-Z", "")
 end
 
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+  float.ceil
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+  float.floor
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
+  date.strftime("%d/%m/%Y")
 end
 
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
+
 end
 
 # capitalize the first letter in each word of a string,
@@ -177,12 +192,14 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
+  string.split.map(&:capitalize)*' '
 end
 
 # return true if a string contains any special characters
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
